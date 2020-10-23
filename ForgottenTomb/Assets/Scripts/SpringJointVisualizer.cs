cffde7466a1acaecb903143462118cef382a4e12
Bryce Suzuki <brycesuzuki@berkeley.edu>
2020-10-22 22:10:59 -0700

    Added omnidirectional dash and velocity

---
+++ ForgottenTomb/Assets/Scripts/SpringJointVisualizer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpringJointVisualizer : MonoBehaviour
+{
+
+    private LineRenderer renderer;
+    private SpringJoint2D springJoint;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        renderer = GetComponent<LineRenderer>();
+        springJoint = GetComponent<SpringJoint2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        renderer.SetPositions(new Vector3[] { transform.position, springJoint.connectedBody.transform.position});
+    }
+}
---
+++ ForgottenTomb/Assets/Scripts/UltraSimpleCameraControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UltraSimpleCameraControl : MonoBehaviour
+{
+    private GameObject playerGO;
+
+    private Camera camera;
+
+    [SerializeField]
+    private float cameraOffset;
+
+
+    void Start()
+    {
+        playerGO = GameObject.FindGameObjectWithTag("Player");
+        camera = GetComponent<Camera>();
+        camera.orthographicSize = cameraOffset;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = new Vector3(playerGO.transform.position.x, playerGO.transform.position.y, this.transform.position.z);
+        
+    }
+}
