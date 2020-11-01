08bd2d7801cb99d1fed5fe00cd1ea516ca472062
Celine Nguyen <celine@berkeley.edu>
2020-10-31 22:33:05 -0700

    Janky level 1

---
+++ ForgottenTomb/Assets/Scripts/Spikes.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            Debug.Log("Player takes damage!");
+            //collision.gameObject.SendMessage("ApplyDamage", 10);
+        }
+    }
+}
