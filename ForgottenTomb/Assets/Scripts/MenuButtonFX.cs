0fa024ef576322a4e4d5aeb3539e36f109ef9ae2
Celine Nguyen <celine@berkeley.edu>
2020-11-19 14:06:40 -0800

    Better Title Menu

---
+++ ForgottenTomb/Assets/Scripts/MenuButtonFX.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButtonFX : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource fx;
+
+    [SerializeField]
+    private AudioClip hoverFx;
+
+    [SerializeField]
+    private AudioClip clickFx;
+
+    public void HoverSound() 
+    {
+        fx.PlayOneShot(hoverFx);
+    }
+
+    public void ClickSound() 
+    {
+        Debug.Log("Plays click sound");
+        fx.PlayOneShot(clickFx);
+    }
+}
