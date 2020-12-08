051611bf74ca82b994b669f14a21dd3043df2af7
Bryce Suzuki <brycesuzuki@berkeley.edu>
2020-12-07 23:22:09 -0800

    Adding boulder functionality

---
+++ ForgottenTomb/Assets/Scripts/Boulder.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boulder : MonoBehaviour, Resettable
+{
+    private Vector3 initialPosition;
+    private Quaternion initialRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        FindObjectOfType<CheckpointManager>().AddToThingsToReset(this);
+        initialPosition = transform.position;
+        initialRotation = transform.rotation;
+    }
+
+    public void Reset()
+    {
+        transform.position = initialPosition;
+        transform.rotation = initialRotation;
+    }
+}
--- ForgottenTomb/Assets/Scripts/BreakAway.cs
+++ ForgottenTomb/Assets/Scripts/BreakAway.cs
@@ -2,14 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BreakAway : MonoBehaviour
+public class BreakAway : MonoBehaviour, Resettable
 {
+    private void Start()
+    {
+        FindObjectOfType<CheckpointManager>().AddToThingsToReset(this);
+    }
+
     void OnCollisionEnter2D(Collision2D coll)
     {
         if (coll.gameObject.tag == "Boulder")
         {
-            Debug.Log("game object destroyed");
-            Destroy(gameObject);
+            //Debug.Log("game object destroyed");
+            gameObject.SetActive(false);
+            //Destroy(gameObject);
         }
     }
 
@@ -17,8 +23,15 @@ public class BreakAway : MonoBehaviour
     {
         if (coll.gameObject.tag == "Boulder")
         {
-            Debug.Log("game object destroyed");
-            Destroy(gameObject);
+            //Debug.Log("game object destroyed");
+            //Destroy(gameObject);
+            gameObject.SetActive(false);
         }
     }
+
+    public void Reset()
+    {
+        gameObject.SetActive(true);
+    }
+
 }
